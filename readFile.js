33548925b5fb4ab5fffb4bd7c07af27e468bd81c
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-15 11:10:11 -0800

    Successfully retreives from database

--- readFile.js
+++ readFile.js
@@ -3,10 +3,10 @@ var saveToDB = function(Servers, json) {
   var servers = new Servers({ servers: json });
   servers.save(function (err, servers) {
     if (err) console.log(err);
-    console.log('SAVED:' + servers);
+    // console.log('SAVED:' + servers);
 
     Servers.find(function (err, servers) {
-      if (err) // TODO handle err
+      if (err) console.log(err);// TODO handle err
       console.log('RETRIEVED:' + servers);
     });
   });
@@ -15,13 +15,8 @@ var saveToDB = function(Servers, json) {
 module.exports = function(Servers) {
   var fs = require('fs');
   var mockDir = './mockData/out/';
-  // var data = {};
-  debugger;
   fs.readdir(mockDir, function(err, files){
-    debugger;
     if (err) throw err;
-    // var c = 0;
-
     files.forEach(function(file){
       fs.readFile(mockDir + file, 'utf-8', function(err, json) {
         if (err) throw err;
