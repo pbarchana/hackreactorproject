caf7ceebd17edd8ef398e7d3215e3898f65a8f9a
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-15 09:59:03 -0800

    Successfully saves to DB

--- readFile.js
+++ readFile.js
@@ -1,32 +1,31 @@
 
+var saveToDB = function(Servers, json) {
+  var servers = new Servers({ servers: json });
+  servers.save(function (err, servers) {
+    if (err) console.log(err);
+    console.log('SAVED:' + servers);
+
+    Servers.find(function (err, servers) {
+      if (err) // TODO handle err
+      console.log('RETRIEVED:' + servers);
+    });
+  });
+};
+
 module.exports = function(Servers) {
   var fs = require('fs');
-  var mockDir = './mockData/out';
+  var mockDir = './mockData/out/';
   // var data = {};
   debugger;
   fs.readdir(mockDir, function(err, files){
     debugger;
     if (err) throw err;
     // var c = 0;
-    var servers = new Servers({ servers: servers });
-    servers.save(function (err, servers) {
-      if (err) console.log(err);
-      console.log('SAVED:' + servers);
 
-      Servers.find(function (err, servers) {
-        if (err) // TODO handle err
-        console.log('RETRIEVED:' + servers);
-      });
-    });
     files.forEach(function(file){
-      // c++;
       fs.readFile(mockDir + file, 'utf-8', function(err, json) {
         if (err) throw err;
-        debugger;
-        // data[file] = html;
-        // if (0 === --c) {
-        //   console.log(data);  //socket.emit('init', {data: data});
-        // }
+        saveToDB(Servers, json);
       });
     });
   });
