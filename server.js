e82bf830cbdcac504fb977664227e6a926aee9c0
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-14 20:42:00 -0800

    added basic express server that calls readFile when mongoose connection is successful

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-// var http = require("http");
-// var handler = require("./request-handler");
-
-// var port = 8080;
-// var ip = "127.0.0.1";
-// var server = http.createServer(handler.handleRequest);
-// console.log("Listening on http://" + ip + ":" + port);
-// server.listen(port, ip);
-
 var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config/config');
+var readFiles = require('./readFile');
 var app = express();
+
+// Setup our database schema and model
+var Schema = mongoose.Schema;
+var dataSchema = new Schema ({ servers: {} });
+var Servers = mongoose.model('Servers', dataSchema);
+
+// Bootstrap db connection
+mongoose.connect(config.db);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // once connection open, read all the mock files and save it to database
+  readFiles(Servers);
+});
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+console.log("Listening on http://localhost:8080");
+app.listen(8080);
