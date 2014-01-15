d8cac77e0a5972502a8943a0a6463f0f7168ad48
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-15 14:56:15 -0800

    Basic server complete

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,12 @@
 
 module.exports = function(app, Servers) {
   
+  // TODO: return index.html
   app.get('/', function(req, res){
     res.send('hello world');
   });
 
+  // get server data
   app.get('/data', function(req, res) {
     // lean returns a plain javascript object with not mongoose stuff atached to it
     Servers.find().lean().exec(function (err, servers) {
@@ -13,7 +15,9 @@ module.exports = function(app, Servers) {
       res.set("Content-Type", "application/json");
       res.send(servers);
     });
-    
   });
+
+  // save server data
+
 };
 
