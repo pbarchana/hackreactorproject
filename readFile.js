25b0b9763aeb185f1236bfd43131e050b7f8c58a
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-15 12:26:03 -0800

    Added more structure to database schema, and successfully query data after retrieval.

--- readFile.js
+++ readFile.js
@@ -1,9 +1,15 @@
 
+var clearDatabase = function(Servers) {
+  Servers.remove({}, function(err) {
+    console.log('collection removed');
+  });
+};
+
 var saveToDB = function(Servers, json) {
-  var servers = new Servers({ servers: json });
+  var servers = new Servers(JSON.parse(json));
   servers.save(function (err, servers) {
     if (err) console.log(err);
-    // console.log('SAVED:' + servers);
+    console.log('SAVED:' + servers);
 
     Servers.find(function (err, servers) {
       if (err) console.log(err);// TODO handle err
@@ -15,6 +21,9 @@ var saveToDB = function(Servers, json) {
 module.exports = function(Servers) {
   var fs = require('fs');
   var mockDir = './mockData/out/';
+  // clear any collections data from the database
+  clearDatabase(Servers);
+  // readfiles each file in the mockData/out dir and save them to the DB
   fs.readdir(mockDir, function(err, files){
     if (err) throw err;
     files.forEach(function(file){
--- server.js
+++ server.js
@@ -6,12 +6,23 @@ var app = express();
 
 // Setup our database schema and model
 var Schema = mongoose.Schema;
-var dataSchema = new Schema ({ servers: {} });
+var dataSchema = new Schema ({
+  date: Number,
+  attributes: {},
+  components: {}
+});
+
+// Setup the model
 var Servers = mongoose.model('Servers', dataSchema);
+// clear any existing data from the model
+// Servers.remove({}, function(err) {
+//   console.log('collection removed');
+// });
 
 // Bootstrap db connection
 mongoose.connect(config.db);
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   // once connection open, read all the mock files and save it to database
