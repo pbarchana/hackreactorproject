8dde15c07c4dd5c5bf4f6efa8679ba99f311be49
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-17 12:12:14 -0800

    grunt generate successfully generates mock data

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,46 @@
 module.exports = function(grunt) {
+  // Helpers
+  var random = function(max) {
+    return Math.ceil(Math.random() * max);
+  };
+
   grunt.initConfig({
-    // start mongo db
     shell: {
+      // start mongo db
       mongo: {
         command: 'mongod',
         options: {
           async: true
         }
+      },
+
+      // Generate mock data
+      generateNodes: {
+        command: 'python mockNodes.py ' + random(20),
+        options: {
+          execOptions: {
+            cwd: 'workers' // selected directory
+          }
+        }
+      },
+      generateSwitches: {
+        command: 'node mockSwitches.js ' + random(5),
+        options: {
+          execOptions: {
+            cwd: 'workers'
+          }
+        }
+      },
+      generateConnectivity: {
+        command: 'node mockConnectivity.js',
+        options: {
+          execOptions: {
+            cwd: 'workers'
+          }
+        }
       }
     },
+
     // start server
     nodemon: {
       dev: {
@@ -24,6 +56,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell-spawn');
   grunt.loadNpmTasks('grunt-nodemon');
 
+  // "grunt generate" - creates all mock data
+  grunt.registerTask('generate', ['shell:generateNodes', 'shell:generateSwitches', 'shell:generateConnectivity']);
+
   // The tasks that get executed when typing 'grunt'
-  grunt.registerTask('default', ['shell', 'nodemon']);
+  grunt.registerTask('default', ['shell:mongo', 'nodemon']);
 };
