39bf7f11f91664dc3e9b8315a93c47a85a6ddf78
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-17 16:04:03 -0800

    Changed index to reference new javascripts/ directory

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,7 +3,6 @@ app.controller('dataCtrl', function($scope, $http){
     method: 'GET',
     url: '/all'
   }).success(function(data) {
-    debugger;
     $scope.servers = data.results.servers;
     $scope.switches = data.results.switches;
   });
