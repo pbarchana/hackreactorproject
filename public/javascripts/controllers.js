8c599717eb28bd8ecd8f92143cb3eb3e08ff6cec
pbarchana <pb_archana@yahoo.com>
2014-01-18 15:51:00 -0800

    controller modified to handle services

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,9 +1,18 @@
-app.controller('dataCtrl', function($scope, $http){
-  $http({
-    method: 'GET',
-    url: '/all'
-  }).success(function(data) {
+
+
+app.controller('d3Controller', function($scope, NetworkDataService) {
+  $rootScope.chartData = [21,1,5];
+  $scope.nodeData = [100, 200];
+});
+
+
+
+app.controller('dataCtrl', function($scope, NetworkDataService){
+  NetworkDataService.getAll()
+  .then(function(data) {
     $scope.servers = data.results.servers;
     $scope.switches = data.results.switches;
-  });
+  }, function errorFunction(reason) {
+    $scope.error = reason;
+  })
 });
