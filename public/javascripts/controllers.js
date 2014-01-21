00a14e0f90a1f10d765cea4c88514b0ef8fc0f82
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-20 16:18:19 -0800

    Fixed table styling issues

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -19,21 +19,9 @@ app.controller('dataCtrl', function($scope, NetworkDataService){
 app.controller('AccordionDemoCtrl', function($scope) {
   $scope.oneAtATime = false;
 
-  $scope.groups = [
-    {
-      title: "Dynamic Group Header - 1",
-      content: "Dynamic Group Body - 1"
-    },
-    {
-      title: "Dynamic Group Header - 2",
-      content: "Dynamic Group Body - 2"
-    }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
+  $scope.isPrimitive = function(value) {
+    if (Array.isArray(value)) return false;
+    if (typeof value === 'object') return false;
+    return true;
   };
 });
