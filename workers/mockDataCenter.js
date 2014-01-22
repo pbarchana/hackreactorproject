b5ffd9dc1e480017334e31770ee6573b95efc611
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-22 14:29:38 -0800

    Created node script to generate mock data center

---
+++ workers/mockDataCenter.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+// Random name from the ones listed below
+var generateName = function() {
+  var names = ['XO San Francisco', 'Cogent', 'Virtustream SF', 'Savvis SN1', 'Colocation America'];
+  var randomIdx = Math.floor(Math.random() * names.length);
+  return names[randomIdx];
+};
+
+// Generate coordinate within 2 points of given number
+var generateCoordinate = function(num) {
+  return  num + (Math.random() * 2);
+};
+
+var generateDataCenter = function() {
+  return {
+    type: 'datacenter',
+    name: generateName(),
+    longitude: generateCoordinate(120),
+    latitude: generateCoordinate(36)
+  };
+};
+
+generateDataCenter();
--- workers/mockSwitches.js
+++ workers/mockSwitches.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+
 var generateMAC = function() {
   var mac = "";
 
