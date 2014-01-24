44dd2a168c9bfe404f660dffaad053abfee516b6
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-23 21:14:10 -0800

    D3 shows up on google map!!!!!!!!!!!s

--- public/client/directives/directives.js
+++ public/client/directives/directives.js
@@ -113,7 +113,6 @@ app.directive('helloMaps', ['d3Service', function(d3Service) {
     link: function (scope, elem, attrs) {
       d3Service.d3().then(function(d3) {
         // generate map
-        debugger;
         var mapOptions, map;
         mapOptions = {
           zoom: 8,
@@ -126,11 +125,46 @@ app.directive('helloMaps', ['d3Service', function(d3Service) {
         var data = scope.nwdata;
         var overlay = new google.maps.OverlayView();
         overlay.onAdd = function() {
-          debugger;
-          console.log('arrived');
+          console.log('layer added');
+          // select overlay and add a div element
           var layer = d3.select(this.getPanes().overlayLayer).append("div")
               .attr("class", "stations");
+
+          overlay.draw = function() {
+            console.log('drawing');
+            var projection = this.getProjection(),
+                padding = 10;
+
+            var marker = layer.selectAll("svg")
+                .data(d3.entries(data))
+                .each(transform) // update existing markers
+              .enter().append("svg:svg")
+                .each(transform)
+                .attr("class", "marker");
+
+            // Add a circle.
+            marker.append("svg:circle")
+                .attr("r", 4.5)
+                .attr("cx", padding)
+                .attr("cy", padding);
+
+            // Add a label.
+            // marker.append("svg:text")
+            //     .attr("x", padding + 7)
+            //     .attr("y", padding)
+            //     .attr("dy", ".31em")
+            //     .text(function(d) { return d.key; });
+
+            function transform(d) {
+              d = new google.maps.LatLng(d.value.latitude, d.value.longitude);
+              d = projection.fromLatLngToDivPixel(d);
+              return d3.select(this)
+                  .style("left", (d.x - padding) + "px")
+                  .style("top", (d.y - padding) + "px");
+            }
+          };
         };
+
         overlay.setMap(map);
       });
     }
