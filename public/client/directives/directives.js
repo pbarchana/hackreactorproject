c181fa84e91802a3846e9251d184dee2f96e0e3c
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-23 18:15:26 -0800

    Added added restric and scope  to map directive

--- public/client/directives/directives.js
+++ public/client/directives/directives.js
@@ -84,6 +84,52 @@ var bootstrapd3 = function(scope, element, attrs, d3Service) {
 
 };
 
+
+
+app.directive('networkGraph', ['d3Service', function(d3Service) {
+  return {
+    restrict: 'EA',
+    scope: {
+      nwdata: '=',
+      loading: '='
+    },
+    link: function(scope, element, attrs) {
+      debugger;
+      setTimeout(function() {
+        bootstrapd3(scope, element, attrs, d3Service);
+      }, 1000);
+      // bootstrapd3(scope, element, attrs, d3Service);
+    }
+  };
+}]);
+
+app.directive('helloMaps', function () {
+  return {
+    restrict: 'EA',
+    scope: {
+      nwdata: '=',
+      loading: '='
+    },
+    link: function (scope, elem, attrs) {
+      var mapOptions,
+        latitude = attrs.latitude,
+        longitude = attrs.longitude,
+        map;
+
+      latitude = latitude && parseFloat(latitude, 10) || 43.074688;
+      longitude = longitude && parseFloat(longitude, 10) || -89.384294;
+
+      mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(latitude, longitude)
+      };
+
+      map = new google.maps.Map(elem[0], mapOptions);
+    }
+  };
+});
+
+
 // var bootstrapMap = function(scope, element, attrs, d3Service) {
 //   // Create the Google Map…
 //   debugger;
@@ -151,40 +197,3 @@ var bootstrapd3 = function(scope, element, attrs, d3Service) {
 //   //   overlay.setMap(map);
 //   });
 // };
-
-app.directive('networkGraph', ['d3Service', function(d3Service) {
-  return {
-    restrict: 'EA',
-    scope: {
-      nwdata: '=',
-      loading: '='
-    },
-    link: function(scope, element, attrs) {
-      debugger;
-      setTimeout(function() {
-        bootstrapd3(scope, element, attrs, d3Service);
-      }, 1000);
-      // bootstrapd3(scope, element, attrs, d3Service);
-    }
-  };
-}]);
-
-app.directive('helloMaps', function () {
-  return function (scope, elem, attrs) {
-    debugger;
-    var mapOptions,
-      latitude = attrs.latitude,
-      longitude = attrs.longitude,
-      map;
-
-    latitude = latitude && parseFloat(latitude, 10) || 43.074688;
-    longitude = longitude && parseFloat(longitude, 10) || -89.384294;
-
-    mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(latitude, longitude)
-    };
-
-    map = new google.maps.Map(elem[0], mapOptions);
-  };
-});
