40d1b104dcb3ceb784115d002deca530bd7bf6c0
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-26 18:13:07 -0800

    gulp file works

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var exec = require('child_process').exec;
+var async = require('async');
+
+gulp.task('generate', function() {
+  var callback = function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  };
+  exec('node' + __dirname + '/workers/checkForDirectories', callback);
+  exec('node' + __dirname + '/workers/deleteMockData', callback);
+  exec('node' + __dirname + '/workers/mockDataCenter 2', callback);
+  exec('python' + __dirname + '/workers/mockNodes.py 2', callback);
+  exec('node' + __dirname + '/workers/mockSwitches 2', callback);
+  exec('node' + __dirname + '/workers/mockConnectivity', callback);
+  exec('node' + __dirname + '/workers/saveFilesToDB', callback);
+});
+
+gulp.task('save', function() {
+});
+
+gulp.task('connect', function() {
+});
+
 
 gulp.task('default', function(){
   // place code for your default task here
