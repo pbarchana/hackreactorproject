ce4301ee4bf1e1d0f8b4fb7c3ad6ea522f61c584
pbarchana <pb_archana@yahoo.com>
2014-01-28 21:56:09 -0800

    select nodes

--- public/client/directives/zoomHelpers.js
+++ public/client/directives/zoomHelpers.js
@@ -1,38 +1,29 @@
 var d3 = require('d3');
 
 module.exports.macToArcMapping = function(arc) {
-          var arcMap = d3.map();
-          var allNodes = d3.selectAll(".node");
-          var node;
-          allNodes[0].forEach(function(n){
-          	
-            node = d3.select(n);
+  var arcMap = d3.map();
+  d3.selectAll(".node")[0].forEach(function(n){
+  	
+    var node = d3.select(n);
 
-            var centerX = d3.select(n)[0][0].__data__.x;
-            var centerY = d3.select(n)[0][0].__data__.y;
-            var centroidX;
-            var centroidY;
-            var ptX;
-            var ptY;
+    var centerX = d3.select(n)[0][0].__data__.x;
+    var centerY = d3.select(n)[0][0].__data__.y;
 
-            for (var i = 0; i < n.childNodes.length; i++) {
-              var segment = d3.select(n.childNodes[i]);
-              var seg = segment[0][0].__data__;
-              centroidX = arc.centroid(seg)[0];
-              centroidY = arc.centroid(seg)[1];
+    for (var i = 0; i < n.childNodes.length; i++) {
+      var segment = d3.select(n.childNodes[i]);
+      var seg = segment[0][0].__data__;
+      var centroidX = arc.centroid(seg)[0];
+      var centroidY = arc.centroid(seg)[1];
 
-              ptX = centroidX + centerX;
-              ptY = centroidY + centerY;
-
-              arcMap.set(d3.select(n.childNodes[i]).datum().data.mac,
-                         {arc: d3.select(n.childNodes[i]).datum(),
-                          x: ptX,
-                          y: ptY
-                        });
-            }
-          });
-          return arcMap;
-      };
+      arcMap.set(d3.select(n.childNodes[i]).datum().data.mac,
+                 {arc: d3.select(n.childNodes[i]).datum(),
+                  x: centroidX + centerX,
+                  y: centroidY + centerY
+                });
+    }
+  });
+  return arcMap;
+};
 
 module.exports.definePie = function(){
   var pie = d3.layout.pie()
@@ -61,7 +52,70 @@ module.exports.createSvg = function(view, width, height){
 }
 
 module.exports.redraw = function() {
-      svg.attr('transform', 'translate(' + d3.event.translate + ')'
-        + ' scale(' + d3.event.scale + ')');
-    };
+  svg.attr('transform', 'translate(' + d3.event.translate + ')'
+    + ' scale(' + d3.event.scale + ')');
+};
+
+module.exports.showNodeInfo = function(node, that){
+  var selected = d3.select(that).attr('nodeSelected');
+  if(selected === 'false'){
+    d3.select(that).style('stroke', '#bada55');
+  }
+
+  var showNode = that;
+  for (var i = 0; i < that.childNodes.length; i++) {
+    d3.select(that.childNodes[i]).datum().hiliteLink = 'true'
+  }
+  d3.selectAll(".link").transition()
+    .style("stroke", function(l) {
+      if (l.source.arc.hiliteLink === 'true' ||
+          l.target.arc.hiliteLink === 'true') {
+            return "black";
+      }
+      else {
+        return "#999";
+      }
+    })
+    .style("stroke-opacity", function(l) {
+      if (l.source.arc.hiliteLink === 'true' ||
+          l.target.arc.hiliteLink === 'true') {
+            return 1.0;
+      }
+      else {
+        return 0.1;
+      }
+  });
+};
+
+module.exports.hideNodeInfo = function(showNode) {
+  var that = showNode;
+  for (var i = 0; i < that.childNodes.length; i++) {
+    d3.select(that.childNodes[i]).datum().hiliteLink = 'false';
+  }
+  d3.selectAll(".link").transition()
+    .style("stroke", function(l) {
+      if (l.source.arc.hiliteLink === 'true' ||
+          l.target.arc.hiliteLink === 'true') {
+            return "#999";
+      }
+    })
+    .style("stroke-opacity", function(l) {
+      if (l.source.arc.hiliteLink === 'true' ||
+          l.target.arc.hiliteLink === 'true') {
+            return 0.1;
+      }
+  });
+  showNode = "";
+};
+
+module.exports.createForceLayout = function(width, height){
+  var force = d3.layout.force()
+  .charge(-2000)
+  .linkStrength(0.1)
+  .linkDistance(70)
+  .gravity(0.3)
+  .size([width, height]);
+  return force;
+};
+
 
--- public/client/directives/zoomIn.js
+++ public/client/directives/zoomIn.js
@@ -1,16 +1,12 @@
 var zoomHelpers = require('./zoomHelpers.js');
 var angular = require('angular');
-//var helpers = require('./helpers.js');
 
 var d3 = require('d3');
 
 var app = angular.module('app');
 
-
 var bootstrapd3 = function(scope,  element, attrs) {
   setTimeout(function(){
-
-    
 
     //View window width and height
     var viewWidth = element[0].offsetWidth;
@@ -23,26 +19,11 @@ var bootstrapd3 = function(scope,  element, attrs) {
     var radius = 45;
     var color = d3.scale.category20c();
 
-
     var pie = zoomHelpers.definePie();
     var arc = zoomHelpers.defineArc(radius);
     var svg = zoomHelpers.createSvg(element[0], viewWidth, viewHeight);
-
+    var force = zoomHelpers.createForceLayout(viewWidth - 200, viewHeight - 200);
     
-
-    var force = d3.layout.force()
-      .charge(-2000)
-      .linkStrength(0.1)
-      .linkDistance(70)
-      .gravity(0.3)
-      .size([viewWidth - 200, viewHeight - 200]);
-
-    //Create view window SVG
-    
-
-    var selectedArc;
-    var selectedArc1;
-
     force
     .links(scope.nwdata.links)
     .nodes(scope.nwdata.nodes)
@@ -51,70 +32,12 @@ var bootstrapd3 = function(scope,  element, attrs) {
     for(var i = scope.nwdata.nodes * scope.nwdata.nodes; i > 0; --i) {
        force.tick();
     }
-
     var showNode;
- 
-    var showNodeInfo = function(node, that){
-      console.log("Node", node);
-      //debugger;
-      var selected = d3.select(that).attr('nodeSelected');
-
-      if(selected === 'false'){
-        d3.select(that).style('stroke', '#bada55');
-      }
-
-      showNode = that;
-      for (var i = 0; i < that.childNodes.length; i++) {
-        d3.select(that.childNodes[i]).datum().hiliteLink = 'true'
-      }
-      d3.selectAll(".link").transition()
-        .style("stroke", function(l) {
-          if (l.source.arc.hiliteLink === 'true' ||
-              l.target.arc.hiliteLink === 'true') {
-                return "black";
-          }
-          else {
-            return "#999";
-          }
-        })
-        .style("stroke-opacity", function(l) {
-          if (l.source.arc.hiliteLink === 'true' ||
-              l.target.arc.hiliteLink === 'true') {
-                return 1.0;
-          }
-          else {
-            return 0.1;
-          }
-      });
-    };
-
-    var hideNodeInfo = function() {
-      var that = showNode;
-      for (var i = 0; i < that.childNodes.length; i++) {
-        d3.select(that.childNodes[i]).datum().hiliteLink = 'false'
-      }
-      d3.selectAll(".link").transition()
-        .style("stroke", function(l) {
-          if (l.source.arc.hiliteLink === 'true' ||
-              l.target.arc.hiliteLink === 'true') {
-                return "#999";
-          }
-        })
-        .style("stroke-opacity", function(l) {
-          if (l.source.arc.hiliteLink === 'true' ||
-              l.target.arc.hiliteLink === 'true') {
-                return 0.1;
-          }
-      });
-
-      showNode = "";
-    }
-
-
-
+    var selectedArc;
+    var selectedArc1;
+    
     setTimeout(function(){
       force.stop()
-
       var point = {};
       var point1 = {};
       var node = svg.selectAll(".node")
@@ -123,8 +46,8 @@ var bootstrapd3 = function(scope,  element, attrs) {
         .classed('node', true)
         .on('mouseenter', function(d) {
           var that = this;
-          showNodeInfo(d, that)})
-        .on('mouseleave', hideNodeInfo);
+          zoomHelpers.showNodeInfo(d, that)})
+        .on('mouseleave', zoomHelpers.hideNodeInfo);
 
       node.selectAll("path")
         .data(function(d) {return pie(d.components.nics); })
@@ -135,10 +58,6 @@ var bootstrapd3 = function(scope,  element, attrs) {
           var centerY = d3.select(this.parentNode).node().transform.animVal.getItem(0).matrix.f;
           var centroidX = arc.centroid(d)[0];
           var centroidY = arc.centroid(d)[1];
-          // console.log("Centroid X", centroidX);
-          // console.log("Centroid X", centroidY);
-          // console.log("Center X", centerX);
-          // console.log("Center Y", centerY);
 
           point1.x = point.x;
           point1.y = point.y;
@@ -160,11 +79,8 @@ var bootstrapd3 = function(scope,  element, attrs) {
             testLink.target.x = point.x;
             testLink.target.y = point.y;
 
-            //console.log("Testlink", testLink);
-
             testLinkArray.push(testLink);
             force.links().push(testLink);
-            //console.log(testLinkArray);
 
             svg.append('g').selectAll(".link")
             .data(testLinkArray)
@@ -174,9 +90,6 @@ var bootstrapd3 = function(scope,  element, attrs) {
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; })
             .classed('link', true)
-            // .attr("class", "link")
-            // .style("stroke-width", "1px")
-            // .style("stroke", "black")
           }
 
       })
@@ -193,16 +106,14 @@ var bootstrapd3 = function(scope,  element, attrs) {
           linkDirectory[a + "," + b] = 1;
           linkDirectory[b + "," + a] = 1;
         };
-
       
       var map = zoomHelpers.macToArcMapping(arc);
       var tempNode;
 
       for (var k = 0;  k < scope.nwdata.links.length; k++) {
         var l = scope.nwdata.links[k];
-        // console.log("l = ",l.source);
         tempNode = l.source.arc;
-        //console.log(" map = ",map," temp = ", tempNode);
+
         l.source["arc"] = map.get(tempNode).arc;
         l.source.x   = map.get(tempNode).x;
         l.source.y   = map.get(tempNode).y;
@@ -212,7 +123,7 @@ var bootstrapd3 = function(scope,  element, attrs) {
         l.target.x   = map.get(tempNode).x;
         l.target.y   = map.get(tempNode).y;
       }
-      // console.log("s", scope.nwdata.links[0]);
+
       var link = svg.selectAll(".link")
         .data(scope.nwdata.links)
         .enter().append("line")
@@ -245,7 +156,6 @@ app.directive('zoomIn', [function() {
       setTimeout(function() {
         bootstrapd3(scope, element, attrs);
       }, 1000);
-      // bootstrapd3(scope, element, attrs, d3Service);
     }
   };
 }]);
