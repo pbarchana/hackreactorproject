7d3bd4f696786c872d914688166842c38676c16a
pbarchana <pb_archana@yahoo.com>
2014-01-29 16:05:55 -0800

    modularized zoom view

--- public/client/directives/zoomHelpers.js
+++ public/client/directives/zoomHelpers.js
@@ -75,7 +75,7 @@ module.exports.redraw = function() {
         + ' scale(' + d3.event.scale + ')');
     };
 
-module.exports.showNodeInfo = function(node, that){
+var showNodeInfo = function(node, that){
       console.log("Node", node);
       //debugger;
       var selected = d3.select(that).attr('nodeSelected');
@@ -110,7 +110,7 @@ module.exports.showNodeInfo = function(node, that){
     };
 
 
-module.exports.hideNodeInfo = function() {
+var hideNodeInfo = function() {
       var that = showNode;
       for (var i = 0; i < that.childNodes.length; i++) {
         d3.select(that.childNodes[i]).datum().hiliteLink = 'false'
@@ -131,4 +131,80 @@ module.exports.hideNodeInfo = function() {
 
       showNode = "";
     }
+
+module.exports.nodeActions = function(scope, force, pie, arc, svg){
+      var point = {}, prevArc, currentSelectedArc;
+      var node = svg.selectAll(".node")
+        .data(scope.nwdata.nodes)
+        .enter().append("g")
+        .classed('node', true)
+        .on('mouseenter', function(d) {
+          var that = this;
+          showNodeInfo(d, that)})
+        .on('mouseleave', hideNodeInfo);
+
+
+      node.selectAll("path")
+        .data(function(d) {return pie(d.components.nics); })
+        .enter().append("svg:path")
+        .attr("d", arc)
+        .on('click', function(d){
+          var centerX = d3.select(this.parentNode).node().transform.animVal.getItem(0).matrix.e;
+          var centerY = d3.select(this.parentNode).node().transform.animVal.getItem(0).matrix.f;
+          var centroidX = arc.centroid(d)[0];
+          var centroidY = arc.centroid(d)[1];
+          
+          prevArc = currentSelectedArc;
+          currentSelectedArc  = d;
+
+          if (currentSelectedArc !== undefined && prevArc !== undefined && prevArc !== currentSelectedArc) {      
+            var testLink = {};
+            testLink.source = prevArc;
+            testLink.source.x = point.x;
+            testLink.source.y = point.y;
+            testLink.target = currentSelectedArc;
+            testLink.target.x = centerX + centroidX;
+            testLink.target.y = centerY + centroidY;           
+            point = testLink.target;
+
+            force.links().push(testLink);
+            svg.append('g').selectAll(".link")
+            .data([testLink])
+            .enter().insert("line", ".node")
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; })
+            .classed('link', true)
+       }
+      })
+      .append("title").text(function(d, i) { return "MAC: " + d.data.mac; })
+      .style("fill", function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+      node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+    };
+    
+module.exports.drawLinks = function(scope, svg){
+
+   var link = svg.selectAll(".link")
+        .data(scope.nwdata.links)
+        .enter().append("line")
+        .classed('link', true)
+        .style("stroke-width", "1px")
+        .style("stroke", "black")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+  return link;
+};
+
+
+
+
+
+
+
+
+
+
 
--- public/client/directives/zoomIn.js
+++ public/client/directives/zoomIn.js
@@ -1,20 +1,13 @@
 var zoomHelpers = require('./zoomHelpers.js');
 var angular = require('angular');
-//var helpers = require('./helpers.js');
-
 var d3 = require('d3');
 
 var app = angular.module('app');
-
 
 var bootstrapd3 = function(scope,  element, attrs) {
 
     var viewWidth = element[0].offsetWidth;
     var viewHeight = element[0].offsetHeight;
-    var linkDirectory = {};
-    var selected_link = null;
-    var link;
-    var node;
 
     var radius = 45;
     var color = d3.scale.category20c();
@@ -22,11 +15,7 @@ var bootstrapd3 = function(scope,  element, attrs) {
     var pie = zoomHelpers.definePie();
     var arc = zoomHelpers.defineArc(radius);
     var svg = zoomHelpers.createSvg(element[0], viewWidth, viewHeight);
-    var force = zoomHelpers.createForceLayout(viewWidth-200, viewHeight-200);
-    
-
-    var selectedArc;
-    var selectedArc1;
+    var force = zoomHelpers.createForceLayout(viewWidth-200, viewHeight-200);    
 
     force
     .links(scope.nwdata.links)
@@ -37,78 +26,16 @@ var bootstrapd3 = function(scope,  element, attrs) {
        force.tick();
     }
 
-    var showNode;
     setTimeout(function(){
       force.stop()
 
-      var point = {};
-      var point1 = {};
-      var node = svg.selectAll(".node")
-        .data(scope.nwdata.nodes)
-        .enter().append("g")
-        .classed('node', true)
-        .on('mouseenter', function(d) {
-          var that = this;
-          zoomHelpers.showNodeInfo(d, that)})
-        .on('mouseleave', zoomHelpers.hideNodeInfo);
-
-      node.selectAll("path")
-        .data(function(d) {return pie(d.components.nics); })
-        .enter().append("svg:path")
-        .attr("d", arc)
-        .on('click', function(d){
-          var centerX = d3.select(this.parentNode).node().transform.animVal.getItem(0).matrix.e;
-          var centerY = d3.select(this.parentNode).node().transform.animVal.getItem(0).matrix.f;
-          var centroidX = arc.centroid(d)[0];
-          var centroidY = arc.centroid(d)[1];
-          point1.x = point.x;
-          point1.y = point.y;
-          point.x = centerX + centroidX;
-          point.y = centerY + centroidY;
-
-          selectedArc1 = selectedArc;
-          selectedArc  = d;
-
-          if (selectedArc !== undefined && selectedArc1 !== undefined && selectedArc1 !== selectedArc) {
-          
-            var testLink = {};
-            var testLinkArray = [];
-            testLink.source = selectedArc1
-            testLink.target = selectedArc;
-            testLink.source.x = point1.x;
-            testLink.source.y = point1.y;
-
-            testLink.target.x = point.x;
-            testLink.target.y = point.y;
-
-            testLinkArray.push(testLink);
-            force.links().push(testLink);
-            svg.append('g').selectAll(".link")
-            .data(testLinkArray)
-            .enter().insert("line", ".node")
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; })
-            .classed('link', true)
-       }
-
-      })
-      .append("title").text(function(d, i) {
-       var retString = 
-        "MAC: " + d.data.mac;
-        return retString;
-      })
-      .style("fill", function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
-
-      node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
-
+     zoomHelpers.nodeActions(scope, force, pie, arc, svg);
+      
       var map = zoomHelpers.macToArcMapping(arc);
-      var tempNode;
 
       for (var k = 0;  k < scope.nwdata.links.length; k++) {
         var l = scope.nwdata.links[k];
-        tempNode = l.source.arc;
+        var tempNode = l.source.arc;
         l.source["arc"] = map.get(tempNode).arc;
         l.source.x   = map.get(tempNode).x;
         l.source.y   = map.get(tempNode).y;
@@ -119,16 +46,7 @@ var bootstrapd3 = function(scope,  element, attrs) {
         l.target.y   = map.get(tempNode).y;
       }
 
-      var link = svg.selectAll(".link")
-        .data(scope.nwdata.links)
-        .enter().append("line")
-        .classed('link', true)
-        .style("stroke-width", "1px")
-        .style("stroke", "black")
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; })
+     var link = zoomHelpers.drawLinks(scope, svg);
 
       scope.$apply(function() {
         scope.loading = false;
